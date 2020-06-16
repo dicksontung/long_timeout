55a4617283655de98bf0e02f471088677b5d84b7
dickson <dickson.tung@qumonintelligence.com>
2020-06-16 17:59:34 +0800

    Add kubernetes

--- client/main.go
+++ client/main.go
@@ -12,17 +12,26 @@ import (
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
+var conns = flag.Int("conns", 1, "number of connection")
+var timeout = flag.Duration("timeout", 120 * time.Second, "timeout in seconds")
 
 func main() {
 	flag.Parse()
-	log.SetFlags(0)
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
 	log.Printf("connecting to %s", u.String())
+	for i := 0; i < *conns-1; i++ {
+		go ws(u, interrupt)
+	}
+	ws(u, interrupt)
+}
 
+func ws(u url.URL, interrupt chan os.Signal) {
+	log.Printf("dialing")
+	websocket.DefaultDialer.HandshakeTimeout = *timeout
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
 		log.Fatal("dial:", err)
